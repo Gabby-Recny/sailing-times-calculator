0d2023c1c880471e049b37a0f298f6f07d31bbba
Gabby Recny <gjtrecny@gmail.com>
2022-05-09 20:55:53 -0500

    Create and style result title

---
+++ src/Result/Result.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Result.scss';
+
+const Result = () => {
+    return (
+        <section className='result-container'>
+            <h5 className='result-title'>Result</h5>
+        </section>
+    )
+}
+
+export default Result;
